7a5e8d02334b484ab2bdb6b8cd5dc135489ab9ec
reza <rezakurnia1902@gmail.com>
2024-07-16 14:05:30 +0700

    delete proguard in fav

--- favorite/build.gradle.kts
+++ favorite/build.gradle.kts
@@ -16,16 +16,6 @@ android {
         minSdk = 28
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
